69c89e6a9d81f244d61ba5b407bc32603ac6cabf
Emilio Jose Mena Cebrian <emenaceb@gmail.com>
2013-10-21 03:26:41 +0200

    ModuleName pattern for module filtering

---
+++ src/main/java/jenkins/plugins/maveninfo/util/InvalidPatternException.java
@@ -0,0 +1,9 @@
+package jenkins.plugins.maveninfo.util;
+
+public class InvalidPatternException extends RuntimeException {
+
+	public InvalidPatternException(String message) {
+		super(message);
+	}
+
+}
